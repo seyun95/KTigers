24af935df6b1aa1c5172871a337935bcab6ba28d
hansong <guudtree@gmal.com>
2022-10-17 22:09:19 +0900

    hansong function

--- baseball.py
+++ baseball.py
@@ -9,26 +9,35 @@ import random
 # 6. 3S 이면 게임 끝
 
 
-def make_correct_number(min, max):   
+def make_correct_number():   
     correct_numbers = []
-    correct_numbers = random.sample(range(min, max), 3)#세자릿수 만들어 정의하기
+    correct_numbers = random.sample(range(1, 10), 3)#세자릿수 만들어 정의하기
     #print("숫자를 임력하세요. 같은 숫자 안됨. 예: 123")#안내문구 표시
     #anserd = input()#입력 받기
     #return anserd, correct_numbers
     return correct_numbers
 
-def set_value():
-    strike2 = 3
-    boll = 0
-    chance = 0
-    index_anserd = 0 #인데스로 대답을 하나씩 비교할때 쓰는 변수
-    #return strike, boll, chance, index_anserd
+def input_anserd():
+    anserd = input()
+    print("111-", anserd)
+    return anserd
 
-def check_answer(correct_numbers, anserd):
+#def set_value():
+ #   strike2 = 3
+ # boll = 0
+ # chance = 0
+ #index_anserd = 0 #인데스로 대답을 하나씩 비교할때 쓰는 변수
+ #return strike, boll, chance, index_anserd
+
+def play_baseball(correct_numbers):
     strike = 0
+    index_anserd = 0
     boll = 0
     chance = 0
+    print("숫자를 임력하세요. 같은 숫자 안됨. 예: 123")#안내문구 표시
+    anserd = input_anserd()
     while strike < 3:
+        print("anserd : ", anserd)
         if anserd == 'q':
             print('실패하셨습니다.')
             print('정답:',correct_numbers)
@@ -51,7 +60,8 @@ def check_answer(correct_numbers, anserd):
             index_anserd = 0 #인데스로 대답을 하나씩 비교할때 쓰는 변수
             print('시도한 횟수:',chance,'(그만하고 싶으시면 q라고 쓰세요)')
             print("숫자를 임력하세요 예: 123")#안내문구 표시
-            anserd = input()#입력 받기
+            anserd = input_anserd()
+
 
     if strike == 3:       
         print('시도한 횟수:',chance)
@@ -59,7 +69,15 @@ def check_answer(correct_numbers, anserd):
 
 #input_correct_anserd(0, 0)
 
-num1 = make_correct_number(1,9);
+#m1 = 100
+#m2 = 200
+#input_anserd()
+#make_correct_number()
+#m1 = m1 + 20
+#anserd = input_anserd()
+correct_numbers = make_correct_number()
+#num2 = check_answer(num1, 0)
+
 #num2 = make_correct_number(60,90);
-print ("num1 :", num1)
-check_answer(num1)
+#print ("num1 :",num1)
+play_baseball(correct_numbers)
